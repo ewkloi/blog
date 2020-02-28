5d35fae0dffb40b4f598f6978c9fd01401684e07
hanfeng <chenyongchang00@126.com>
2020-02-28 21:45:25 +0800

    b

--- src/test/java/cn/edu/test/utilsTest.java
+++ src/test/java/cn/edu/test/utilsTest.java
@@ -1,12 +1,9 @@
 package cn.edu.test;
 
-import cn.edu.service.BlogService;
-import cn.edu.utils.Md5Utils;
-import cn.edu.utils.StringUtils;
-import cn.edu.vo.IndexBlog;
+
 import org.junit.Test;
 import org.junit.runner.RunWith;
-import org.springframework.beans.factory.annotation.Autowired;
+
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
 
